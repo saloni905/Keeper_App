a6b8afcc7d9b1ef4a0bfc46b3bbefd43b7dd5721
saloni905 <103562251+saloni905@users.noreply.github.com>
2023-07-25 23:39:50 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,70 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 
-ReactDOM.render(
-  
-    <App />,
-  
-  document.getElementById('root')
-)
+import express from "express"
+import cors from "cors"
+import mongoose from "mongoose"
 
+
+
+const app = express()
+app.use(express.urlencoded())
+app.use(express.json())
+app.use(cors())
+
+mongoose.connect("mongodb://localhost:27017/mykeeperAppDB",{useNewUrlParser: true, useUnifiedTopology: true}, () => console.log("DB connected") )
+
+const keeperSchema = mongoose.Schema({
+    title: String,
+    description: String
+})
+
+const keeper = new mongoose.model("keeper",keeperSchema)
+
+app.get("/api/getAll",(req, res) =>{
+    keeper.find({}, (err, keeperList) => {
+        if(err){
+            console.log(err)
+        } else {
+            res.status(200).send(keeperList)
+        }
+    })
+})
+
+app.post("/api/addNew",(req, res) => {
+    const {title, description } = req.body
+    const keeperObj = new keeper({
+        title,
+        description
+    })
+    
+    keeperObj.save( err =>{
+        if(err){
+            console.log(err)
+        }
+        keeper.find({}, (err, keeperList) => {
+            if(err){
+                console.log(err)
+            } else {
+                res.status(200).send(keeperList)
+            }
+        })
+    })
+})
+
+
+app.post("/api/delete",(req, res) =>{
+    const { id } = req.body
+    keeper.deleteOne({_id: id}, () =>{
+        keeper.find({}, (err,keeperList ) => {
+        if(err){
+            console.log(err)
+        } else {
+            res.status(200).send(keeperList)
+         }
+       })
+    })
+})
+
+app.listen( 3001, () => {
+    console.log("Backend created at port 3001")
+})
 
