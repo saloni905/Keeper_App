4adcccebdabf0de875cfd7cf0945290b1c33fc28
saloni905 <103562251+saloni905@users.noreply.github.com>
2023-07-25 23:36:36 +0530

    Add files via upload

---
+++ header.js
@@ -0,0 +1,14 @@
+import React from "react"
+import "./header.css"
+import HighlightIcon from "@material-ui/icons/Highlight";
+
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1><HighlightIcon/> The Keeper App</h1>
+        </div>
+    )
+}
+
+export default Header
